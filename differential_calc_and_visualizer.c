9eea0fd44f5db31a8d074181db3204172b24d0c2
Kanivan-S <skanivan12@gmail.com>
2022-01-20 20:22:10 +0530

    parser comparison

--- differential_calc_and_visualizer.c
+++ differential_calc_and_visualizer.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 #include "stack.h"
 
-int derivativeVal()
+int derivativeVal(stack poly, int val)
 {
   long int ans = 0;
 
   return ans;
 }
 
+stack parse(char str[200])
+{
+  char tempstr[100];
+  printf("%s\n", str);
+  for (int i = 0; i < strlen(str); i++)
+  {
+    printf("%c\n", str[i]);
+    char one=str[i];
+    if (one=='+' || one=='-')
+    {
+      // input.push(tempstr);
+      // char tempstr[100] = "\0";
+    }
+    else
+    {
+      strcat(tempstr, &str[i]);
+      printf("%s\n", tempstr);
+    }
+  }
+}
+
 int main()
 {
-
+  char str[200]="2x+3x";
+//   scanf("%[^\n]%*c", str);
+  parse(str);
   stack input = create();
   stack output = create();
 
