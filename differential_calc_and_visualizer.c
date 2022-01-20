a9feee24d56af073d88f72939676c6886e599f7d
balajianbalagan <vijibalaji2003@gmail.com>
2022-01-20 18:40:42 +0530

    stack function

--- differential_calc_and_visualizer.c
+++ differential_calc_and_visualizer.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "stack.h"
+
+stack fillstack(char *input)
+{
+}
+
 int main()
 {
-  stack s = createstack(5);
-  push(s, 10);
-  push(s, 20);
-  displaystack(s);
+
+  stack stack = createstack(5);
 
   return 0;
 }
