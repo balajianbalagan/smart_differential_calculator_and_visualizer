fdb56371af581d5160c908ba53f1e933a610d742
balajianbalagan <vijibalaji2003@gmail.com>
2022-01-27 20:57:32 +0530

    differentiate perfected final array

--- postfix_diferentiation/differentiate.h
+++ postfix_diferentiation/differentiate.h
@@ -26,49 +26,49 @@ node differentiate(tree etree)
     else if (etree->fn == 3 && strcmp(etree->info, "+"))
     {
         insertEnd(diff, differentiate(etree->left_child));
-        insertEnd(diff, "+");
+        insertEnd(diff, "+", 3); //0-numbers 1-functions(sin,cos) 2-variable (x) 3-operators 4-open brackets 5-close brackets404-not defined yet
         insertEnd(diff, differentiate(etree->right_child));
     }
     else if (etree->fn == 3 && strcmp(etree->info, "-"))
     {
-        insertEnd(diff, differentiate(etree->left_child));
-        insertEnd(diff, "-");
-        insertEnd(diff, differentiate(etree->right_child));
+        insertEnd(diff, differentiate(etree->left_child), 1);
+        insertEnd(diff, "-", 3);
+        insertEnd(diff, differentiate(etree->right_child), 1);
     }
     else if (etree->fn == 3 && strcmp(etree->info, "*"))
     {
-        insertEnd(diff, inorder(etree->left_child));
-        insertEnd(diff, differentiate(etree->right_child));
-        insertEnd(diff, "+");
-        insertEnd(diff, inorder(etree->right_child));
-        insertEnd(diff, differentiate(etree->left_child));
+        insertEnd(diff, inorder(etree->left_child), 1);
+        insertEnd(diff, differentiate(etree->right_child), 1);
+        insertEnd(diff, "+", 3);
+        insertEnd(diff, inorder(etree->right_child), 1);
+        insertEnd(diff, differentiate(etree->left_child), 1);
     }
     else if (etree->fn == 3 && strcmp(etree->info, "/"))
     {
-        insertEnd(diff, inorder(etree->left_child));
-        insertEnd(diff, differentiate(etree->right_child));
-        insertEnd(diff, "-");
-        insertEnd(diff, inorder(etree->right_child));
-        insertEnd(diff, differentiate(etree->left_child));
-        insertEnd(diff, "/");
-        insertEnd(diff, "(");
-        insertEnd(diff, inorder(etree->right_child));
-        insertEnd(diff, ")");
-        insertEnd(diff, "^");
-        insertEnd(diff, "2");
+        insertEnd(diff, inorder(etree->left_child), 1);
+        insertEnd(diff, differentiate(etree->right_child), 1);
+        insertEnd(diff, "-", 3);
+        insertEnd(diff, inorder(etree->right_child), 1);
+        insertEnd(diff, differentiate(etree->left_child), 1);
+        insertEnd(diff, "/", 3);
+        insertEnd(diff, "(", 4);
+        insertEnd(diff, inorder(etree->right_child), 1);
+        insertEnd(diff, ")", 5);
+        insertEnd(diff, "^", 3);
+        insertEnd(diff, "2", 0);
     }
     else if (etree->fn == 3 && strcmp(etree->info, "^"))
     {
-        insertEnd(diff, etree->right_child->info);
-        insertEnd(diff, "*");
-        insertEnd(diff, "(");
-        insertEnd(diff, etree->left_child->info);
-        insertEnd(diff, "(");
-        insertEnd(diff, etree->right_child->info - 1);
-        insertEnd(diff, ")");
-        insertEnd(diff, ")");
-        insertEnd(diff, "*");
-        insertEnd(diff, differentiate(etree->left_child));
+        insertEnd(diff, etree->right_child->info, 1);
+        insertEnd(diff, "*", 3);
+        insertEnd(diff, "(", 4);
+        insertEnd(diff, etree->left_child->info, 1);
+        insertEnd(diff, "(", 4);
+        insertEnd(diff, etree->right_child->info - 1, 1);
+        insertEnd(diff, ")", 5);
+        insertEnd(diff, ")", 5);
+        insertEnd(diff, "*", 3);
+        insertEnd(diff, differentiate(etree->left_child), 1);
     }
 }
 
